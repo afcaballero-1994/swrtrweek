97d7e04939ae00706ae36a78d84d84823fe7ab2d
Andres Pedraza <andrespedca@gmail.com>
2024-04-18 20:10:25 -0500

    normal calculations added

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,22 +1,31 @@
 import Foundation
 
 func rayColor(ray: Ray<Float>) -> Vec3<Float>{
-    if isSphereHit(center: Vec3<Float>(x: 0, y: 0, z: -1), radius: 0.8, ray: ray){
-        return Vec3<Float>(x: 1, y: 0, z: 0)
+    let t: Float = sphereHit(center: Vec3<Float>(x: 0, y: 0, z: -1), radius: 0.5, ray: ray)
+
+    if t > 0{
+        let normal: Vec3<Float> = (ray.at(t: t) - Vec3<Float>(x: 0, y: 0, z: -1)).normalize()
+
+        return 0.5 * Vec3<Float>(x: normal.x + 1, y: normal.y + 1, z: normal.z + 1)
     }
+
     let unitDirection: Vec3<Float> = ray.direction.normalize()
     let a: Float = 0.5 * (unitDirection.y + 1.0)
 
-    return (1 - a) * Vec3<Float>(x: 1, y: 1, z: 1) + a * Vec3<Float>(x: 0.5, y: 0.7, z: 1.0)
+    return ((1 - a) * Vec3<Float>(x: 1, y: 1, z: 1)) + (a * Vec3<Float>(x: 0.5, y: 0.7, z: 1))
 }
 
-func isSphereHit(center: Vec3<Float>, radius: Float, ray: Ray<Float>) -> Bool{
+func sphereHit(center: Vec3<Float>, radius: Float, ray: Ray<Float>) -> Float{
     let oc: Vec3<Float> = center - ray.origin
     let a: Float = ray.direction * ray.direction
     let b: Float = -2 * ray.direction * oc
     let c: Float = oc * oc - radius * radius
     let discriminant: Float = b*b - 4*a*c
-    return discriminant >= 0
+    if discriminant < 0{
+        return -1
+    } else{
+        return (-b - sqrtf(discriminant)) / (2 * a)
+    }
 }
 
 func main(){
@@ -24,8 +33,8 @@ func main(){
     let height = 9 * 30
 
     let kamera: Camera = Camera()
-    // kamera.viewportHeight = 2
-    // kamera.viewporthWidth = kamera.viewportHeight * 16 / 9
+    //kamera.viewportHeight = 8
+    //kamera.viewporthWidth = 3
 
     var imgData: String = ""
     imgData.reserveCapacity(width * height * 12)
