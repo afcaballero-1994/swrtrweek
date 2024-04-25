dcc3a985b6335cf0aedc97b0b69c59d87af23bfd
Andres Pedraza <andrespedca@gmail.com>
2024-04-25 14:11:21 -0500

    changed generation scene code

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,27 +1,57 @@
 import Foundation
 
 func main(){
-    let width = 16 * 30
-    let height = 9 * 30
+    let width = 16 * 80
+    let height = 9 * 80
 
     let kamera: Camera = Camera(imageWidth: width, imageHeight: height, 
-                                    samplesPerPixel: 100, maxDepth: 50, vFov: 45,
-                                    lookFrom: Vec3<Float>(x: -2, y: 2, z: 1),
-                                    lookAt: Vec3<Float>(x: 0, y: 0, z: -1)
+                                    samplesPerPixel: 500, maxDepth: 50, vFov: 45,
+                                    lookFrom: Vec3<Float>(x: 13, y: 2, z: 3),
+                                    lookAt: Vec3<Float>(x: 0, y: 0, z: 0),
+                                    defocusAngle: 0.6,
+                                    focusDist: 10
                                 )
 
     var world: HittableList = HittableList()
 
-    let materialGround: Lambertian = Lambertian(albedo: Vec3<Float>(x: 0.8, y: 0.8, z: 0))
-    world.add(object: Sphere(center: Vec3(x: 0, y: -1000, z: -1), radius: 100, material: materialGround))
-    let materialCenter: Lambertian = Lambertian(albedo: Vec3<Float>(x: 0.1, y: 0.2, z: 0.5))
-    let materialLeft: Dielectric = Dielectric(ir: 1.5)
-    let materialRight: Metal = Metal(albedo: Vec3<Float>(x: 0.8, y: 0.6, z: 0.2), fuzziness: 0)
-
+    let materialGround: Lambertian = Lambertian(albedo: Vec3<Float>(x: 0.5, y: 0.5, z: 0.5))
+    world.add(object: Sphere(center: Vec3(x: 0, y: -1000, z: 0), radius: 1000, material: materialGround))
     
-    world.add(object: Sphere(center: Vec3(x: 0, y: 0, z: -1.2), radius: 0.5, material: materialCenter))
-    world.add(object: Sphere(center: Vec3(x: -1, y: 0, z: -1), radius: 0.4, material: materialLeft))
-    world.add(object: Sphere(center: Vec3(x: 1, y: 0, z: -1), radius: 0.5, material: materialRight))
+    for a in -11..<11{
+        for b in -11..<11{
+            let matChoose = Float.random(in: 0..<1)
+            let center: Vec3<Float> = Vec3<Float>(x: Float(a) + 0.9 * Float.random(in: 0..<1), 
+                                    y: 0.2, 
+                                    z: Float(b) + 0.9 * Float.random(in: 0..<1))
+            
+            if (center - Vec3<Float>(x: 4, y: 0.2, z: 0)).magnitude() > 0.9{
+                var material: Material
+
+                if matChoose < 0.8{
+                    let albedo: Vec3<Float> = Vec3<Float>.generateRandomVec3() .* Vec3<Float>.generateRandomVec3()
+                    material = Lambertian(albedo: albedo)
+                    world.add(object: Sphere(center: center, radius: 0.2, material: material))
+                } else if matChoose < 0.95{
+                    let albedo: Vec3<Float> = Vec3<Float>.generateRandomVec3()
+                    let fuzz: Float = Float.random(in: 0..<0.5)
+                    material = Metal(albedo: albedo, fuzziness: fuzz)
+                    world.add(object: Sphere(center: center, radius: 0.2, material: material))
+                } else{
+                    material = Dielectric(ir: 1.5)
+                    world.add(object: Sphere(center: center, radius: 0.2, material: material))
+                }
+            }
+        }
+    }
+
+    let material1: Dielectric = Dielectric(ir: 1.5)
+    world.add(object: Sphere(center: Vec3<Float>(x: 0, y: 1, z: 0), radius: 1, material: material1))
+
+    let material2: Lambertian = Lambertian(albedo: Vec3<Float>(x: 0.4, y: 0.2, z: 0.1))
+    world.add(object: Sphere(center: Vec3<Float>(x: -4, y: 1, z: 0), radius: 1, material: material2))
+
+    let material3: Metal = Metal(albedo: Vec3<Float>(x: 0.7, y: 0.6, z: 0.5), fuzziness: 0)
+    world.add(object: Sphere(center: Vec3<Float>(x: 4, y: 1, z: 0), radius: 1, material: material3))
     
     kamera.render(world: world)
 
